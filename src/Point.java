42bcd216b21f39a0f28b3ea101928de47c6fc50a
nexus85 <mx38@mail.ru>
2016-08-28 13:14:19 +0300

    ttt

---
+++ src/Point.java
@@ -0,0 +1,23 @@
+/**
+ * Created by nexus on 28.08.16.
+ */
+public class Point {
+
+    public int x;
+    public int y;
+    public char sym;
+
+    public Point() {
+    }
+
+
+    public Point(int x, int y, char sym) {
+        this.x = x;
+        this.y = y;
+        this.sym = sym;
+    }
+
+    public void Draw() {
+
+    }
+}
